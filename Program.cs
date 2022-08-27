4314518c136a5416fb7e85748baf4734b391d736
Austin Hamilton <HAMILTONAJ@ETSU.EDU>
2022-08-27 18:04:07 -0400

    Enhance the App

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {date:d} at {date:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
